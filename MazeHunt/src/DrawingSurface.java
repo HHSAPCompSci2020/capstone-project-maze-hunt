b31f67085fc4021cad423027745b3596007cc86f
fdominique576 <70028686+fdominique576@users.noreply.github.com>
2021-05-05 13:20:46 -0700

    update

--- MazeHunt/src/DrawingSurface.java
+++ MazeHunt/src/DrawingSurface.java
@@ -1,4 +1,20 @@
 import processing.core.PApplet;
 public class DrawingSurface extends PApplet{
-
+	
+	
+	public DrawingSurface() {
+		
+	}
+	
+	public void draw() { 
+		background(255);   // Clear the screen with a white background
+		fill(0);
+		textAlign(LEFT);
+		textSize(12);
+		
+		text("Enter: Run 1 step\nSpace: Start/Stop\nUp arrow: Increase speed\nDown arrow: Decrease speed\n\nSpeed: " + " per sec", height+20, 30);
+		
+		
+		
+	}
 }
