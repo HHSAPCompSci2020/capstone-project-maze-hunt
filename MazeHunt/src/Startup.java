c2a46562737cab2276fa9a74f84327d8f8651626
fdominique576 <70028686+fdominique576@users.noreply.github.com>
2021-05-05 13:18:19 -0700

    startup

--- MazeHunt/src/Startup.java
+++ MazeHunt/src/Startup.java
@@ -1,5 +1,38 @@
+import java.awt.Dimension;
+import java.io.IOException;
+
+import javax.swing.JFrame;
+import javax.swing.JOptionPane;
+
+import processing.awt.PSurfaceAWT;
+import processing.core.PApplet;
+import java.awt.Dimension;
+
+import javax.swing.JFrame;
+
+import processing.awt.PSurfaceAWT;
+import processing.core.PApplet;
+
 
 public class Startup {
 //test
+
 	
+	
+	
+	public static void main(String[] args) throws IOException {
+		
+		DrawingSurface drawing = new DrawingSurface();
+		PApplet.runSketch(new String[]{""}, drawing);
+		PSurfaceAWT surf = (PSurfaceAWT) drawing.getSurface();
+		PSurfaceAWT.SmoothCanvas canvas = (PSurfaceAWT.SmoothCanvas) surf.getNative();
+		JFrame window = (JFrame)canvas.getFrame();
+
+		window.setSize(400, 300);
+		window.setMinimumSize(new Dimension(100,100));
+		window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		window.setResizable(true);
+
+		window.setVisible(true);
+	}
 }
