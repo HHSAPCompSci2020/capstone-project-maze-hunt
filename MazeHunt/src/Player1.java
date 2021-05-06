c9d665704ae7800c177569e2ae2711c2c1730932
saloning <70005546+saloning@users.noreply.github.com>
2021-05-05 17:03:54 -0700

    added fields for Player1 and Player2

--- MazeHunt/src/Player1.java
+++ MazeHunt/src/Player1.java
@@ -1,4 +1,10 @@
 
 public class Player1 {
+	
+	int Clues;
+	int numSuperBoost;
+	int numDiamonds;
+	
+	
 
 }
--- MazeHunt/src/Player2.java
+++ MazeHunt/src/Player2.java
@@ -1,4 +1,8 @@
 
 public class Player2 {
+	
+	int Clues;
+	int numSuperBoost;
+	int numDiamonds;
 
 }
