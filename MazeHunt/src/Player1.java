0231e3a69d382ad4dc5a9018ad61339cd594efae
saloning <70005546+saloning@users.noreply.github.com>
2021-05-05 17:13:14 -0700

    added constructor and methods in Player1 and Player2

--- MazeHunt/src/Player1.java
+++ MazeHunt/src/Player1.java
@@ -1,10 +1,33 @@
 
-public class Player1 {
+public class Player1 extends Maze {
 	
+	//fields
 	int Clues;
 	int numSuperBoost;
 	int numDiamonds;
 	
 	
+	//constructor
+	public Player1() {
+		
+	}
+	
+	
+	//methods
+	public void run() {
+		
+	}
+	
+	public void speed() {
+		
+	}
+	
+	public void solveClue() {
+		
+	}
+	
+	public void mazeDetector() {
+		
+	}
 
 }
--- MazeHunt/src/Player2.java
+++ MazeHunt/src/Player2.java
@@ -1,8 +1,33 @@
 
-public class Player2 {
+public class Player2 extends Maze {
 	
+	//fields
 	int Clues;
 	int numSuperBoost;
 	int numDiamonds;
 
+	
+	//constructor
+	public Player2() {
+		
+	}
+	
+	
+	//methods
+	public void run() {
+		
+	}
+	
+	public void speed() {
+		
+	}
+	
+	public void solveClue() {
+		
+	}
+	
+	public void mazeDetector() {
+		
+	}
+	
 }
