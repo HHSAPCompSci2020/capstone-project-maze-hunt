40c6fee74c962fb012e31d202d6debab901bb9c7
Anika <Anikaunq@gmail.com>
2021-05-11 17:13:04 -0700

    Clues added

--- MazeHunt/src/Clues.java
+++ MazeHunt/src/Clues.java
@@ -11,7 +11,13 @@ public class Clues {
 	 */
 	public Clues ()
 	{
-		
+		clues.add("Aha! You want to enter, I don't even know if you'll make it to the center. "
+				+ "Watch out for the cat, as he likes to eat rats. Before I let you enter the maze, what's dessert that may contain glaze.");
+		clues.add("Oh no! You've hit a wall, that's okay stand up tall. To measure your height, walk down a hall.");
+		clues.add("Yay, you found me! You forgot your galsses, but I love your hair. Let me help you see a clearer, walk around the squre to "
+				+ "try to find a mirror.");
+		clues.add("You're almost done, just one more to go. 'Ughh' I know what a bummer! Just tell me your favourite thing to throw and then "
+				+ "you'll have reached the summer!");
 	}
 	
 	/**
