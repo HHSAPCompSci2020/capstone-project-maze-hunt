abf39009cf97a4df0607a285a123f70959d41dc7
saloning <70005546+saloning@users.noreply.github.com>
2021-05-11 20:30:27 -0700

    edited the clues

--- MazeHunt/src/Clues.java
+++ MazeHunt/src/Clues.java
@@ -12,12 +12,17 @@ public class Clues {
 	public Clues ()
 	{
 		clues.add("Aha! You want to enter, I don't even know if you'll make it to the center. "
-				+ "Watch out for the cat, as he likes to eat rats. Before I let you enter the maze, what's dessert that may contain glaze.");
-		clues.add("Oh no! You've hit a wall, that's okay stand up tall. To measure your height, walk down a hall.");
-		clues.add("Yay, you found me! You forgot your galsses, but I love your hair. Let me help you see a clearer, walk around the squre to "
-				+ "try to find a mirror.");
-		clues.add("You're almost done, just one more to go. 'Ughh' I know what a bummer! Just tell me your favourite thing to throw and then "
-				+ "you'll have reached the summer!");
+				+ "\nWatch out for the cat, as he likes to eat rats. "
+				+ "\nBefore I let you enter the maze, what's a dessert that may contain glaze?");
+		clues.add("Oh no! You've hit a wall, that's okay stand up tall. "
+				+ "\nTo measure your height, walk down a hall.");
+		clues.add("Yay, you found me! You forgot your galsses, but I love your hair. "
+				+ "\nLet me help you see a little clearer, "
+				+ "\nwalk around the square to try to find a mirror.");
+		clues.add("You're almost done, just one more to go. "
+				+ "\n'Ughh' I know what a bummer! "
+				+ "\nJust tell me your favourite thing to throw "
+				+ "\nand then you'll have reached the summer!");
 	}
 	
 	/**
