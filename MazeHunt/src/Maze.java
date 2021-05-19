10382c19c42ca15064500c62d447b3c7ae307c72
Anika <Anikaunq@gmail.com>
2021-05-18 19:36:49 -0700

    Added lines to arrayList

--- MazeHunt/src/Maze.java
+++ MazeHunt/src/Maze.java
@@ -120,256 +120,256 @@ public class Maze extends Screen{
 
 		//right line
 		Line2D right = new Line2D.Float(x+w, y, x+w, y+h);
-		float rx1 = (float) right.getX1();
-		float ry1 = (float) right.getY1();
-		float rx2 = (float) right.getX2();
-		float ry2 = (float) right.getY2();
-		surface.line(rx1, ry1, rx2, ry2);
+//		float rx1 = (float) right.getX1();
+//		float ry1 = (float) right.getY1();
+//		float rx2 = (float) right.getX2();
+//		float ry2 = (float) right.getY2();
+		surface.line(getLineX1(right), getLineY1(right), getLineX2(right), getLineY2(right));
 		mazeLines.add(right);
 
 
 		//bottom exit
 		Line2D bottome = new Line2D.Float(x+w, y+h, x+w-gap, y+h); //clue
 		//color
-		float bex1 = (float) bottome.getX1();
-		float bey1 = (float) bottome.getY1();
-		float bex2 = (float) bottome.getX2();
-		float bey2 = (float) bottome.getY2();
-		surface.line(bex1, bey1, bex2, bey2);
+//		float bex1 = (float) bottome.getX1();
+//		float bey1 = (float) bottome.getY1();
+//		float bex2 = (float) bottome.getX2();
+//		float bey2 = (float) bottome.getY2();
+		surface.line(getLineX1(bottome), getLineY1(bottome), getLineX2(bottome), getLineY2(bottome));
 		mazeLines.add(bottome);
 
 
 		//bottom line
 		Line2D bottom = new Line2D.Float(x+w-gap, y+h, x, y+h);
-		float bx1 = (float) bottom.getX1();
-		float by1 = (float) bottom.getY1();
-		float bx2 = (float) bottom.getX2();
-		float by2 = (float) bottom.getY2();
-		surface.line(bx1, by1, bx2, by2);
+//		float bx1 = (float) bottom.getX1();
+//		float by1 = (float) bottom.getY1();
+//		float bx2 = (float) bottom.getX2();
+//		float by2 = (float) bottom.getY2();
+		surface.line(getLineX1(bottom), getLineY1(bottom), getLineX2(bottom), getLineY2(bottom));
 		mazeLines.add(bottom);
 
 
 
 		Line2D tester = new Line2D.Float(x+w-gap, y+h, x, y+h);
 		//Line2D test = new Line2D(x+w-gap, y+h, x, y+h);
-		float x1 = (float) tester.getX1();
-		float y1 = (float) tester.getY1();
-		float x2 = (float) tester.getX2();
-		float y2 = (float) tester.getY2();
-		surface.line(x1, y1, x2, y2);
+//		float x1 = (float) tester.getX1();
+//		float y1 = (float) tester.getY1();
+//		float x2 = (float) tester.getX2();
+//		float y2 = (float) tester.getY2();
+		surface.line(getLineX1(tester), getLineY1(tester), getLineX2(tester), getLineY2(tester));
 		mazeLines.add(tester);
 
 
 		//left line
 		Line2D left = new Line2D.Float(x, y+h, x, y);
-		float lx1 = (float) left.getX1();
-		float ly1 = (float) left.getY1();
-		float lx2 = (float) left.getX2();
-		float ly2 = (float) left.getY2();
-		surface.line(lx1, ly1, lx2, ly2);
+//		float lx1 = (float) left.getX1();
+//		float ly1 = (float) left.getY1();
+//		float lx2 = (float) left.getX2();
+//		float ly2 = (float) left.getY2();
+		surface.line(getLineX1(left), getLineY1(left), getLineX2(left), getLineY2(left));
 		mazeLines.add(left);
 
 
 		//line1
 		Line2D line1 = new Line2D.Float(x+gap, y, x+gap, y+gap);
-		float l1x1 = (float) line1.getX1();
-		float l1y1 = (float) line1.getY1();
-		float l1x2 = (float) line1.getX2();
-		float l1y2 = (float) line1.getY2();
-		surface.line(l1x1, l1y1, l1x2, l1y2);
+//		float l1x1 = (float) line1.getX1();
+//		float l1y1 = (float) line1.getY1();
+//		float l1x2 = (float) line1.getX2();
+//		float l1y2 = (float) line1.getY2();
+		surface.line(getLineX1(line1), getLineY1(line1), getLineX2(line1), getLineY2(line1));
 		mazeLines.add(line1);
 
 
 		//line2
 		Line2D line2 = new Line2D.Float(x+gap*2, y+gap, x+w, y+gap);
-		float l2x1 = (float) line2.getX1();
-		float l2y1 = (float) line2.getY1();
-		float l2x2 = (float) line2.getX2();
-		float l2y2 = (float) line2.getY2();
-		surface.line(l2x1, l2y1, l2x2, l2y2);
+//		float l2x1 = (float) line2.getX1();
+//		float l2y1 = (float) line2.getY1();
+//		float l2x2 = (float) line2.getX2();
+//		float l2y2 = (float) line2.getY2();
+		surface.line(getLineX1(line2), getLineY1(line2), getLineX2(line2), getLineY2(line2));
 		mazeLines.add(line2);
 
 
 		//line3
 		Line2D line3 = new Line2D.Float(x+gap, y+gap*2, x+w-gap, y+gap*2);
-		float l3x1 = (float) line3.getX1();
-		float l3y1 = (float) line3.getY1();
-		float l3x2 = (float) line3.getX2();
-		float l3y2 = (float) line3.getY2();
-		surface.line(l3x1, l3y1, l3x2, l3y2);
+//		float l3x1 = (float) line3.getX1();
+//		float l3y1 = (float) line3.getY1();
+//		float l3x2 = (float) line3.getX2();
+//		float l3y2 = (float) line3.getY2();
+		surface.line(getLineX1(line3), getLineY1(line3), getLineX2(line3), getLineY2(line3));
 		mazeLines.add(line3);
 
 
 		//line4
 		Line2D line4 = new Line2D.Float(x+gap, y+gap*2, x+gap, y+gap*3);
-		float l4x1 = (float) line4.getX1();
-		float l4y1 = (float) line4.getY1();
-		float l4x2 = (float) line4.getX2();
-		float l4y2 = (float) line4.getY2();
-		surface.line(l4x1, l4y1, l4x2, l4y2);
+//		float l4x1 = (float) line4.getX1();
+//		float l4y1 = (float) line4.getY1();
+//		float l4x2 = (float) line4.getX2();
+//		float l4y2 = (float) line4.getY2();
+		surface.line(getLineX1(line4), getLineY1(line4), getLineX2(line4), getLineY2(line4));
 		mazeLines.add(line4);
 
 
 		//line5
 		Line2D line5 = new Line2D.Float(x+gap*2, y+gap*3, x+gap*2, y+gap*4);
-		float l5x1 = (float) line5.getX1();
-		float l5y1 = (float) line5.getY1();
-		float l5x2 = (float) line5.getX2();
-		float l5y2 = (float) line5.getY2();
-		surface.line(l5x1, l5y1, l5x2, l5y2);
+//		float l5x1 = (float) line5.getX1();
+//		float l5y1 = (float) line5.getY1();
+//		float l5x2 = (float) line5.getX2();
+//		float l5y2 = (float) line5.getY2();
+		surface.line(getLineX1(line5), getLineY1(line5), getLineX2(line5), getLineY2(line5));
 		mazeLines.add(line5);
 
 		//line6
 		Line2D line6 = new Line2D.Float(x+gap*3, y+gap*2, x+gap*3, y+gap*3);
-		float l6x1 = (float) line6.getX1();
-		float l6y1 = (float) line6.getY1();
-		float l6x2 = (float) line6.getX2();
-		float l6y2 = (float) line6.getY2();
-		surface.line(l6x1, l6y1, l6x2, l6y2);
+//		float l6x1 = (float) line6.getX1();
+//		float l6y1 = (float) line6.getY1();
+//		float l6x2 = (float) line6.getX2();
+//		float l6y2 = (float) line6.getY2();
+		surface.line(getLineX1(line6), getLineY1(line6), getLineX2(line6), getLineY2(line6));
 		mazeLines.add(line6);
 
 		//line7
 		Line2D line7 = new Line2D.Float(x+gap*4, y+gap*3, x+gap*6, y+gap*3);
-		float l7x1 = (float) line7.getX1();
-		float l7y1 = (float) line7.getY1();
-		float l7x2 = (float) line7.getX2();
-		float l7y2 = (float) line7.getY2();
-		surface.line(l7x1, l7y1, l7x2, l7y2);
+//		float l7x1 = (float) line7.getX1();
+//		float l7y1 = (float) line7.getY1();
+//		float l7x2 = (float) line7.getX2();
+//		float l7y2 = (float) line7.getY2();
+		surface.line(getLineX1(line7), getLineY1(line7), getLineX2(line7), getLineY2(line7));
 		mazeLines.add(line7);
 
 		//line8
 		Line2D line8 = new Line2D.Float(x+gap*7, y+gap*3, x+w, y+gap*3);
-		float l8x1 = (float) line8.getX1();
-		float l8y1 = (float) line8.getY1();
-		float l8x2 = (float) line8.getX2();
-		float l8y2 = (float) line8.getY2();
-		surface.line(l8x1, l8y1, l8x2, l8y2);
+//		float l8x1 = (float) line8.getX1();
+//		float l8y1 = (float) line8.getY1();
+//		float l8x2 = (float) line8.getX2();
+//		float l8y2 = (float) line8.getY2();
+		surface.line(getLineX1(line8), getLineY1(line8), getLineX2(line8), getLineY2(line8));
 		mazeLines.add(line8);
 
 		//line9
 		Line2D line9 = new Line2D.Float(x+gap, y+gap*4, x+gap*3, y+gap*4);
-		float l9x1 = (float) line9.getX1();
-		float l9y1 = (float) line9.getY1();
-		float l9x2 = (float) line9.getX2();
-		float l9y2 = (float) line9.getY2();
-		surface.line(l9x1, l9y1, l9x2, l9y2);
+//		float l9x1 = (float) line9.getX1();
+//		float l9y1 = (float) line9.getY1();
+//		float l9x2 = (float) line9.getX2();
+//		float l9y2 = (float) line9.getY2();
+		surface.line(getLineX1(line9), getLineY1(line9), getLineX2(line9), getLineY2(line9));
 		mazeLines.add(line9);
 
 		//line10
 		Line2D line10 = new Line2D.Float(x+gap*4, y+gap*3, x+gap*4, y+gap*5);
-		float l10x1 = (float) line10.getX1();
-		float l10y1 = (float) line10.getY1();
-		float l10x2 = (float) line10.getX2();
-		float l10y2 = (float) line10.getY2();
-		surface.line(l10x1, l10y1, l10x2, l10y2);
+//		float l10x1 = (float) line10.getX1();
+//		float l10y1 = (float) line10.getY1();
+//		float l10x2 = (float) line10.getX2();
+//		float l10y2 = (float) line10.getY2();
+		surface.line(getLineX1(line10), getLineY1(line10), getLineX2(line10), getLineY2(line10));
 		mazeLines.add(line10);
 
 		//line11
 		Line2D line11 = new Line2D.Float(x+gap*5, y+gap*4, x+gap*6, y+gap*4);
-		float l11x1 = (float) line11.getX1();
-		float l11y1 = (float) line11.getY1();
-		float l11x2 = (float) line11.getX2();
-		float l11y2 = (float) line11.getY2();
-		surface.line(l11x1, l11y1, l11x2, l11y2);
+//		float l11x1 = (float) line11.getX1();
+//		float l11y1 = (float) line11.getY1();
+//		float l11x2 = (float) line11.getX2();
+//		float l11y2 = (float) line11.getY2();
+		surface.line(getLineX1(line11), getLineY1(line11), getLineX2(line11), getLineY2(line11));
 		mazeLines.add(line11);
 
 		//line12
 		Line2D line12 = new Line2D.Float(x+gap*7, (((y+gap*4)+(y+gap*5))/2)-gap/5, x+w-gap*2, (((y+gap*4)+(y+gap*5))/2)-gap/5);
-		float l12x1 = (float) line12.getX1();
-		float l12y1 = (float) line12.getY1();
-		float l12x2 = (float) line12.getX2();
-		float l12y2 = (float) line12.getY2();
-		surface.line(l12x1, l12y1, l12x2, l12y2);
+//		float l12x1 = (float) line12.getX1();
+//		float l12y1 = (float) line12.getY1();
+//		float l12x2 = (float) line12.getX2();
+//		float l12y2 = (float) line12.getY2();
+		surface.line(getLineX1(line12), getLineY1(line12), getLineX2(line12), getLineY2(line12));
 		mazeLines.add(line12);
 
 		//line13
 		Line2D line13 = new Line2D.Float(x+w-gap*2, y+(gap*4-gap/4), x+w-gap*2, y+gap*5);
-		float l13x1 = (float) line13.getX1();
-		float l13y1 = (float) line13.getY1();
-		float l13x2 = (float) line13.getX2();
-		float l13y2 = (float) line13.getY2();
-		surface.line(l13x1, l13y1, l13x2, l13y2);
+//		float l13x1 = (float) line13.getX1();
+//		float l13y1 = (float) line13.getY1();
+//		float l13x2 = (float) line13.getX2();
+//		float l13y2 = (float) line13.getY2();
+		surface.line(getLineX1(line13), getLineY1(line13), getLineX2(line13), getLineY2(line13));
 		mazeLines.add(line13);
 
 		//line14
 		Line2D line14 = new Line2D.Float(x+w-gap, y+gap*3, x+w-gap, y+gap*4);
-		float l14x1 = (float) line14.getX1();
-		float l14y1 = (float) line14.getY1();
-		float l14x2 = (float) line14.getX2();
-		float l14y2 = (float) line14.getY2();
-		surface.line(l14x1, l14y1, l14x2, l14y2);
+//		float l14x1 = (float) line14.getX1();
+//		float l14y1 = (float) line14.getY1();
+//		float l14x2 = (float) line14.getX2();
+//		float l14y2 = (float) line14.getY2();
+		surface.line(getLineX1(line14), getLineY1(line14), getLineX2(line14), getLineY2(line14));
 		mazeLines.add(line14);
 
 		//line15
 		Line2D line15 = new Line2D.Float(x+w-gap, y+(gap*5-gap/6), x+w, y+(gap*5-gap/6));
-		float l15x1 = (float) line15.getX1();
-		float l15y1 = (float) line15.getY1();
-		float l15x2 = (float) line15.getX2();
-		float l15y2 = (float) line15.getY2();
-		surface.line(l15x1, l15y1, l15x2, l15y2);
+//		float l15x1 = (float) line15.getX1();
+//		float l15y1 = (float) line15.getY1();
+//		float l15x2 = (float) line15.getX2();
+//		float l15y2 = (float) line15.getY2();
+		surface.line(getLineX1(line15), getLineY1(line15), getLineX2(line15), getLineY2(line15));
 		mazeLines.add(line15);
 
 		//line16
 		Line2D line16 = new Line2D.Float(x+gap, y+gap*4, x+gap, y+(gap*5+gap/2));
-		float l16x1 = (float) line16.getX1();
-		float l16y1 = (float) line16.getY1();
-		float l16x2 = (float) line16.getX2();
-		float l16y2 = (float) line16.getY2();
-		surface.line(l16x1, l16y1, l16x2, l16y2);
+//		float l16x1 = (float) line16.getX1();
+//		float l16y1 = (float) line16.getY1();
+//		float l16x2 = (float) line16.getX2();
+//		float l16y2 = (float) line16.getY2();
+		surface.line(getLineX1(line16), getLineY1(line16), getLineX2(line16), getLineY2(line16));
 		mazeLines.add(line16);
 
 		//line17
 		Line2D line17 = new Line2D.Float(x+gap*2, y+(gap*5-gap/6), x+gap*3, y+(gap*5-gap/6));
-		float l17x1 = (float) line17.getX1();
-		float l17y1 = (float) line17.getY1();
-		float l17x2 = (float) line17.getX2();
-		float l17y2 = (float) line17.getY2();
-		surface.line(l17x1, l17y1, l17x2, l17y2);
+//		float l17x1 = (float) line17.getX1();
+//		float l17y1 = (float) line17.getY1();
+//		float l17x2 = (float) line17.getX2();
+//		float l17y2 = (float) line17.getY2();
+		surface.line(getLineX1(line17), getLineY1(line17), getLineX2(line17), getLineY2(line17));
 		mazeLines.add(line17);
 
 		//line18
 		Line2D line18 = new Line2D.Float(x+gap*4, y+gap*5, x+gap*6, y+gap*5);
-		float l18x1 = (float) line18.getX1();
-		float l18y1 = (float) line18.getY1();
-		float l18x2 = (float) line18.getX2();
-		float l18y2 = (float) line18.getY2();
-		surface.line(l18x1, l18y1, l18x2, l18y2);
+//		float l18x1 = (float) line18.getX1();
+//		float l18y1 = (float) line18.getY1();
+//		float l18x2 = (float) line18.getX2();
+//		float l18y2 = (float) line18.getY2();
+		surface.line(getLineX1(line18), getLineY1(line18), getLineX2(line18), getLineY2(line18));
 		mazeLines.add(line18);
 
 		//line19
 		Line2D line19 = new Line2D.Float(x+gap*6, y+gap*4, x+gap*6, y+gap*5);//clue
-		float l19x1 = (float) line19.getX1();
-		float l19y1 = (float) line19.getY1();
-		float l19x2 = (float) line19.getX2();
-		float l19y2 = (float) line19.getY2();
-		surface.line(l19x1, l19y1, l19x2, l19y2);
+//		float l19x1 = (float) line19.getX1();
+//		float l19y1 = (float) line19.getY1();
+//		float l19x2 = (float) line19.getX2();
+//		float l19y2 = (float) line19.getY2();
+		surface.line(getLineX1(line19), getLineY1(line19), getLineX2(line19), getLineY2(line19));
 		mazeLines.add(line19);
 
 		//line20
 		Line2D line20 = new Line2D.Float(x, y+(gap*5+gap/2), x+gap,y+(gap*5+gap/2)); //clue
-		float l20x1 = (float) line20.getX1();
-		float l20y1 = (float) line20.getY1();
-		float l20x2 = (float) line20.getX2();
-		float l20y2 = (float) line20.getY2();
-		surface.line(l20x1, l20y1, l20x2, l20y2);
+//		float l20x1 = (float) line20.getX1();
+//		float l20y1 = (float) line20.getY1();
+//		float l20x2 = (float) line20.getX2();
+//		float l20y2 = (float) line20.getY2();
+		surface.line(getLineX1(line20), getLineY1(line20), getLineX2(line20), getLineY2(line20));
 		mazeLines.add(line20);
 
 		//line21
 		Line2D line21 = new Line2D.Float(((x+gap*2)+(x+gap*3))/2, y+h-gap, ((x+gap*2)+(x+gap*3))/2, y+h);
-		float l21x1 = (float) line21.getX1();
-		float l21y1 = (float) line21.getY1();
-		float l21x2 = (float) line21.getX2();
-		float l21y2 = (float) line21.getY2();
-		surface.line(l21x1, l21y1, l21x2, l21y2);
+//		float l21x1 = (float) line21.getX1();
+//		float l21y1 = (float) line21.getY1();
+//		float l21x2 = (float) line21.getX2();
+//		float l21y2 = (float) line21.getY2();
+		surface.line(getLineX1(line21), getLineY1(line21), getLineX2(line21), getLineY2(line21));
 		mazeLines.add(line21);
 
 		//line22
 		Line2D line22 = new Line2D.Float(x+(gap*4-gap/2), y+h-gap+gap/4, x+gap*5, y+h-gap+gap/4);
-		float l22x1 = (float) line22.getX1();
-		float l22y1 = (float) line22.getY1();
-		float l22x2 = (float) line22.getX2();
-		float l22y2 = (float) line22.getY2();
-		surface.line(l22x1, l22y1, l22x2, l22y2);
+//		float l22x1 = (float) line22.getX1();
+//		float l22y1 = (float) line22.getY1();
+//		float l22x2 = (float) line22.getX2();
+//		float l22y2 = (float) line22.getY2();
+		surface.line(getLineX1(line22), getLineY1(line22), getLineX2(line22), getLineY2(line22));
 		mazeLines.add(line22);
 
 		//line23
@@ -378,35 +378,35 @@ public class Maze extends Screen{
 		float l23y1 = (float) line23.getY1();
 		float l23x2 = (float) line23.getX2();
 		float l23y2 = (float) line23.getY2();
-		surface.line(l23x1, l23y1, l23x2, l23y2);
+		surface.line(getLineX1(line23), getLineY1(line23), getLineX2(line23), getLineY2(line23));
 		//surface.line(mazeLines.get(0));
 		mazeLines.add(line23);
 
 		//line24
 		Line2D line24 = new Line2D.Float(x+gap*6, y+h-(gap-gap/4), x+gap*6, y+h);
-		float l24x1 = (float) line24.getX1();
-		float l24y1 = (float) line24.getY1();
-		float l24x2 = (float) line24.getX2();
-		float l24y2 = (float) line24.getY2();
-		surface.line(l24x1, l24y1, l24x2, l24y2);
+//		float l24x1 = (float) line24.getX1();
+//		float l24y1 = (float) line24.getY1();
+//		float l24x2 = (float) line24.getX2();
+//		float l24y2 = (float) line24.getY2();
+		surface.line(getLineX1(line24), getLineY1(line24), getLineX2(line24), getLineY2(line24));
 		mazeLines.add(line24);
 
 		//line25
 		Line2D line25 = new Line2D.Float(x+gap*7, y+h-gap, x+w-gap, y+h-gap);
-		float l25x1 = (float) line25.getX1();
-		float l25y1 = (float) line25.getY1();
-		float l25x2 = (float) line25.getX2();
-		float l25y2 = (float) line25.getY2();
-		surface.line(l25x1, l25y1, l25x2, l25y2);
+//		float l25x1 = (float) line25.getX1();
+//		float l25y1 = (float) line25.getY1();
+//		float l25x2 = (float) line25.getX2();
+//		float l25y2 = (float) line25.getY2();
+		surface.line(getLineX1(line25), getLineY1(line25), getLineX2(line25), getLineY2(line25));
 		mazeLines.add(line25);
 
 		//lastline
 		Line2D lastline = new Line2D.Float(x+w-gap, y+h, x+w-gap, y+h-gap);
-		float llx1 = (float) lastline.getX1();
-		float lly1 = (float) lastline.getY1();
-		float llx2 = (float) lastline.getX2();
-		float lly2 = (float) lastline.getY2();
-		surface.line(llx1, lly1, llx2, lly2);
+//		float llx1 = (float) lastline.getX1();
+//		float lly1 = (float) lastline.getY1();
+//		float llx2 = (float) lastline.getX2();
+//		float lly2 = (float) lastline.getY2();
+		surface.line(getLineX1(lastline), getLineY1(lastline), getLineX2(lastline), getLineY2(lastline));
 		mazeLines.add(lastline);
 
 		//selectClue();
