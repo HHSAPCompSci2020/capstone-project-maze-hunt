f8c474488b7a1f0bad23d0b1a337625e3ef29c96
saloning <70005546+saloning@users.noreply.github.com>
2021-05-18 23:28:34 -0700

    started Clue6

--- MazeHunt/src/Clue6.java
+++ MazeHunt/src/Clue6.java
@@ -1,8 +1,30 @@
+import java.awt.Rectangle;
 
-public class Clue6 {
-
-	public Clue6() {
-		// TODO Auto-generated constructor stub
+public class Clue6 extends Screen{
+	
+	private DrawingSurface surface;
+	
+	public Clue6(DrawingSurface surface) {
+		super(400,300);
+		this.surface = surface;
+	}
+	
+	public void draw() {
+		
+		surface.pushStyle();
+		
+		
+		surface.background(255);   // Clear the screen with a white background
+		surface.stroke(0);     // Set line drawing color to white
+		surface.noFill();
+		
+		surface.fill(0);
+		surface.textSize(9);
+		surface.text("You’ve hit a dead end and you don’t know what to do! "
+				+ "\nMaybe turn around and look for something blue. "
+				+ "\nMake sure you go slow because the clue is in that row. "
+				+ "\nYou may be able to sprint when you find the next hint.",10,30);
+		
 	}
 
 }
