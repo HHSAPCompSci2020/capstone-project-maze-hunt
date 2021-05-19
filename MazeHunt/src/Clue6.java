553dbd933f40c606f48a3bb7dcb214f298d3121d
saloning <70005546+saloning@users.noreply.github.com>
2021-05-19 00:25:04 -0700

    created buttons for Clue6

--- MazeHunt/src/Clue6.java
+++ MazeHunt/src/Clue6.java
@@ -1,12 +1,23 @@
+import java.awt.Point;
 import java.awt.Rectangle;
 
 public class Clue6 extends Screen{
 	
 	private DrawingSurface surface;
+	private Rectangle brownButton;
+	private Rectangle orangeButton;
+	private Rectangle whiteButton;
 	
 	public Clue6(DrawingSurface surface) {
 		super(400,300);
 		this.surface = surface;
+		
+		surface.fill(0);
+		brownButton = new Rectangle(150,200,50,40);
+		surface.fill(0);
+		orangeButton = new Rectangle(150,150,50,40);
+		surface.fill(0);
+		whiteButton = new Rectangle(150,100,50,40);
 	}
 	
 	public void draw() {
@@ -18,13 +29,44 @@ public class Clue6 extends Screen{
 		surface.stroke(0);     // Set line drawing color to white
 		surface.noFill();
 		
+		surface.rect(brownButton.x, brownButton.y, brownButton.width, brownButton.height, 10, 10, 10, 10);
+		surface.rect(orangeButton.x, orangeButton.y, orangeButton.width, orangeButton.height, 10, 10, 10, 10);
+		surface.rect(whiteButton.x, whiteButton.y, whiteButton.width, whiteButton.height, 10, 10, 10, 10);
+		
 		surface.fill(0);
 		surface.textSize(9);
 		surface.text("You've hit a dead end and you don't know what to do! "
 				+ "\nMaybe turn around and look for something blue. "
 				+ "\nMake sure you go slow because the clue is in that row. "
-				+ "\nYou may be able to sprint when you find the next hint.",10,30);
+				+ "\nYou'll be able to sprint when you find the next hint.", 10, 30);
+		
+		surface.text("But before you proceed, let me help you succeed!"
+				+ "\nDon't put on your victory cape just yet,"
+				+ "\nFirst tell me the color of a parisian baguette", 10, 100);
+		
+		surface.textSize(10);
+		surface.fill(0);
+		String Brown = "Brown";
+		float w = surface.textWidth(Brown); //start button
+		surface.text(Brown, brownButton.x+brownButton.width/2-w/2, brownButton.y+brownButton.height/2);
+		String Orange = "Orange";
+		float y = surface.textWidth(Orange); //start button
+		surface.text(Orange, orangeButton.x+orangeButton.width/2-y/2, orangeButton.y+orangeButton.height/2);
+		String White = "White";
+		float z = surface.textWidth(White); //start button
+		surface.text(White, whiteButton.x+whiteButton.width/2-z/2, whiteButton.y+whiteButton.height/2);
+		surface.popStyle();
 		
 	}
+	
+	public void mousePressed() {
+		Point p = surface.actualCoordinatesToAssumed(new Point(surface.mouseX,surface.mouseY));
+		if (brownButton.contains(p))
+			surface.switchScreen(ScreenSwitcher.KEYBOARDPRESS);
+		if (orangeButton.contains(p))
+			surface.switchScreen(ScreenSwitcher.KEYBOARDPRESS);
+		if (whiteButton.contains(p))
+			surface.switchScreen(ScreenSwitcher.KEYBOARDPRESS);
+		}
 
 }
