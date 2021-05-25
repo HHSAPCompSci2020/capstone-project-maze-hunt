5a7c95f7ed68acd6edeb97b2fdddc4011dee5b50
Anika <Anikaunq@gmail.com>
2021-05-25 00:44:43 -0700

    Javadocs added

--- MazeHunt/src/WrongAnswer5.java
+++ MazeHunt/src/WrongAnswer5.java
@@ -1,20 +1,20 @@
-
-
-
 	import java.awt.Point;
 	import java.awt.Rectangle;
-//	import java.awt.event.*;
 
-//	import javax.swing.JOptionPane;
-
-
-
+	/**
+	 * This class holds the screen that pops up after the user chooses the wrong answer for a question.
+	 * @author Anika and Saloni
+	 *
+	 */
 	public class WrongAnswer5 extends Screen {
 		
 		private DrawingSurface surface;
 		private Rectangle startButton;
 
-		
+		/**
+		 * Constructs the wrong answer screen to be shown onto the screen
+		 * @param surface, , the PApplet screen in which the you wrong answer text and the back button will be displayed.
+		 */
 		public WrongAnswer5(DrawingSurface surface) {
 			super(400,300);
 			this.surface = surface;
@@ -22,21 +22,18 @@
 		
 		}
 		
+		/**
+		 * Displays the wrong answer text and back button
+		 */
 		public void draw() {
-			
-			// Draw stuff
-			
 			surface.pushStyle();
-			
 			
 			surface.background(255);   // Clear the screen with a white background
 			surface.stroke(0);     // Set line drawing color to white
 			surface.noFill();
-			
 			//surface.fill(255);
 			surface.fill(204,204,255);
 			surface.rect(startButton.x, startButton.y, startButton.width, startButton.height, 10, 10, 10, 10);
-			
 			
 			surface.fill(0);
 			surface.textSize(9);
@@ -47,18 +44,15 @@
 			String start = "Back";
 			float w = surface.textWidth(start); //start button
 			surface.text(start, startButton.x+startButton.width/2-w/2, startButton.y+startButton.height/2);
-
 			
 			surface.popStyle();
 
-			
-			
-			// Change stuff
-
-		
-
-			
 		}
+		
+		/**
+		 * Checks whether the mouse has pressed the correct answer choices and returns to the maze if the 
+		 * correct answer is chosen else a try again screen appears to let the use know that they got the answer wrong
+		 */
 		public void mousePressed() {
 			Point p = surface.actualCoordinatesToAssumed(new Point(surface.mouseX,surface.mouseY));
 			if (startButton.contains(p))
