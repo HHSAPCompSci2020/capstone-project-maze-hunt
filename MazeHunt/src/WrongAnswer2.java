29fe2164d71c3a12843d28a432541d46f603e1ba
saloning <70005546+saloning@users.noreply.github.com>
2021-05-25 01:29:34 -0700

    fixed formatting

--- MazeHunt/src/WrongAnswer2.java
+++ MazeHunt/src/WrongAnswer2.java
@@ -1,8 +1,5 @@
-
-
-
-	import java.awt.Point;
-	import java.awt.Rectangle;
+import java.awt.Point;
+import java.awt.Rectangle;
 
 	/**
 	 * This class contains the wrong answer2 screen when the wrong answer choice has been selected
@@ -23,7 +20,6 @@
 			super(400,300);
 			this.surface = surface;
 			startButton = new Rectangle(150,200,50,40);
-		
 		}
 		
 		/**
@@ -31,10 +27,7 @@
 		 */
 		public void draw() {
 			
-			// Draw stuff
-			
 			surface.pushStyle();
-			
 			
 			surface.background(255);   // Clear the screen with a white background
 			surface.stroke(0);     // Set line drawing color to white
@@ -42,7 +35,6 @@
 			
 			//surface.fill(255);
 			surface.rect(startButton.x, startButton.y, startButton.width, startButton.height, 10, 10, 10, 10);
-			
 			
 			surface.fill(0);
 			surface.textSize(9);
@@ -54,16 +46,7 @@
 			float w = surface.textWidth(start); //start button
 			surface.text(start, startButton.x+startButton.width/2-w/2, startButton.y+startButton.height/2);
 
-			
 			surface.popStyle();
-
-			
-			
-			// Change stuff
-
-		
-
-			
 		}
 		
 		/**
@@ -76,4 +59,3 @@
 			
 			}
 		}
-
