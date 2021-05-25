c9ae4eb4bb5eb23602f4c41e77f65a546a28a1f4
saloning <70005546+saloning@users.noreply.github.com>
2021-05-24 20:10:24 -0700

    fixed clue2

--- MazeHunt/src/Clue2.java
+++ MazeHunt/src/Clue2.java
@@ -51,7 +51,7 @@ public class Clue2 extends Screen {
 		surface.text("Oh no! You've hit a wall, that's okay stand up tall. "
 				+ "\nTo measure your height, walk down a hall."
 				+ "\nLet me tell you something that I already knew,"
-				+ "\nNow be aware of the lines as you might go through.",10,30);
+				+ "\nNow be aware of the lines as you might move and go through.",10,30);
 		
 		surface.textSize(10);
 		surface.fill(75,0,130);
